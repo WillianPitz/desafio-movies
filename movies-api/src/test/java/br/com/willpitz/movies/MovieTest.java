ccc95f9c47221800e30b150f5a02578f1b79850d
Willian Pitz <willian.pitz@southsystem.com.br>
2024-03-04 13:56:35 -0300

    Removing mocked repository

--- movies-api/src/test/java/br/com/willpitz/movies/MovieTest.java
+++ movies-api/src/test/java/br/com/willpitz/movies/MovieTest.java
@@ -1,47 +1,37 @@
 package br.com.willpitz.movies;
 
 import br.com.willpitz.movies.exception.CsvFileReaderException;
-import br.com.willpitz.movies.persistence.entity.MovieEntity;
 import br.com.willpitz.movies.persistence.repository.MovieRepository;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static br.com.willpitz.movies.utils.TestUtil.assertJsonFromPath;
-import static br.com.willpitz.movies.utils.TestUtil.jsonToObject;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
 class MovieTest {
 
     @Autowired
     private MockMvc webTestClient;
 
-    @MockBean
+    @Autowired
     private MovieRepository repository;
 
     @Test
+    @Order(1)
     void shouldReadCsvAndReturn() throws Exception {
         final var contractPath = "/response/get-movies-response.json";
-        final var findWinners = "/response/find-winners-response.json";
-
-        final var movieEntity = jsonToObject(
-            findWinners,
-            MovieEntity[].class
-        );
-
-        when(repository.findConsecutiveWinners()).thenReturn(Arrays.stream(movieEntity).toList());
 
         webTestClient.perform(get("/movie")
                 .contentType(APPLICATION_JSON))
@@ -50,8 +40,9 @@ class MovieTest {
     }
 
     @Test
+    @Order(2)
     void shouldThrowExceptionWhenFindConsecutiveWinners() throws Exception {
-        when(repository.findConsecutiveWinners()).thenReturn(List.of());
+        repository.deleteAll();
 
         webTestClient.perform(get("/movie")
                 .contentType(APPLICATION_JSON))
